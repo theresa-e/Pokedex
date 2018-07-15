0674813a1d64b99c1320eea2c6f61e9f871013d5
Theresa Eliger <910theresa@gmail.com>
2018-07-15 01:00:11 -0700

    Removed bootstrap, tidied up CSS, changed color scheme.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     // Displays images and assigns an ID
-    for (var i=1; i<=150; i++) {
+    for (var i=1; i<=50; i++) {
         $('.pokemonImages').append('<img id="' + i + 
         '" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + i + '.png">');
     }
@@ -8,18 +8,25 @@ $(document).ready(function(){
     $('img').on('click', function(){
         var pokemonID = event.target.id;
         var pokemonURL = 'https://pokeapi.co/api/v2/pokemon/' + pokemonID;
-        var pokemonImage = '<img class="mx-auto d-block" width ="200px" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + pokemonID + '.png">';
+        var pokemonImage = '<img class="pokeImg" width ="200px" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + pokemonID + '.png">';
         $.get(pokemonURL, function(res) {
             var pokemonName = '<h2 class="text-center">' + res.forms[0].name + '</h2>';
-            var pokemonTypes = '<p class="title">Types:</p><ul>'
+            var pokemonTypes = '<p class="info-title">Type: '
             for (var i = 0; i<res.types.length; i++){
-                pokemonTypes += '<li>' + res.types[i].type.name + '</li>';
+                pokemonTypes += res.types[i].type.name
+                if (i < res.types.length - 1){
+                    pokemonTypes += ", ";
+                } else {
+                    pokemonTypes += ".</p>";
+                }
             }
             pokemonTypes += '</ul>';
-            var pokemonHeight = '<p class="title">Height: </p><ul><li>' + res.height + '</li></ul></p>';
-            var pokemonWeight = '<p class="title">Weight: </p><ul><li>' + res.weight + '</li></ul></p>';
+            var pokemonHeight = '<p class="info-title">Height: ' + res.height + '</p>';
+            var pokemonWeight = '<p class="info-title">Weight: ' + res.weight + '</p>';
+            console.log('name: ', pokemonName)
             console.log(res.height);
             $('.pokemonInfo').html(pokemonImage + pokemonName +  pokemonTypes + pokemonHeight + pokemonWeight);
         }, 'json');
+            $('.pokemonInfo').hide().fadeIn("slow");
         });
 });
